c0ae190fd8f8c1c335dadc97115878a8eb7f2e20
Loctarogar <igrsl1990@gmail.com>
2019-02-18 19:21:24 +0300

    topicShowAll template added

--- Controllers/topic/topicShowAll.php
+++ Controllers/topic/topicShowAll.php
@@ -6,11 +6,21 @@ include_once '../../Core/database.php';
 include_once '../../Objects/topic.php';
 
 $topics = [];
+$counter = 0;
+$arrayCounter = 0;
 
 $database = new Database();
 $conn = $database->getConnection();
 $topic = new Topic($conn);
 $stmt = $topic->topicShowAll();
-$topics = $stmt->fetchAll();
+//$topics = $stmt->fetchAll();
+while($row = $stmt->fetch()){
+    $counter += 1;
+    $topics[$arrayCounter][] = $row;
+    if($counter == 3){
+        $arrayCounter += 1;
+        $counter = 0;
+    }
+}
 
 include_once '../../Templates/topic/topicShowAll.php';
--- Templates/topic/topicShowAll.php
+++ Templates/topic/topicShowAll.php
@@ -1,5 +1,5 @@
-<?php ?>
-
+<?php
+/**
 <html>
 <head>
     <title>Show All Topics</title>
@@ -9,5 +9,52 @@
 <?php foreach ($topics as $topic){ ?><br>
     <a  href="../../Controllers/topic/topicRead.php?topicId=<?php echo $topic['topic_id'] ?>"><?php echo $topic['name']; ?></a>
 <?php } ?>
+</body>
+</html>
+*/
+?>
+
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>White Edition</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="../../public/css/style.css">
+</head>
+<body>
+<header>
+    <nav class="main-nav">
+        <ul>
+            <li>
+                <ul>
+                    <li><a href="index.html" class="active">about</a></li>
+                    <li><a href="projets.html">works</a></li>
+                    <li><a href="contact.html">Contact</a></li>
+                </ul>
+            </li>
+        </ul>
+    </nav>
+</header>
+<section id="video" class="home">
+    <h1>Enjoy our forum</h1>
+</section>
+<section id="main-content">
+    <div class="text-intro">
+        <h2>All Topics</h2>
+    </div>
+    <?php foreach ($topics as $value){ ?>
+    <div class="columns features">
+        <?php foreach($value as $topic){ ?>
+            <div class="one-third">
+                <h3><?php echo $topic['name']; ?></h3>
+                <p><?php echo substr($topic['body'], 0, 200); ?></p>
+                <p><a  href="../../Controllers/topic/topicRead.php?topicId=<?php echo $topic['topic_id'] ?>" class="more">Watch all topic</a></p>
+            </div>
+        <?php } ?>
+    </div>
+    <?php } ?>
+    <footer>
+<div class="copyright"><small>Made by Loctarogar.</small></div>
+</footer>
 </body>
 </html>
