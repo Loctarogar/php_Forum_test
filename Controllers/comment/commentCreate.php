e95a58953295dc18e197823985c69ac3a18e2416
Loctarogar <igrsl1990@gmail.com>
2019-02-28 21:43:05 +0300

    commentCreate permission added

--- Controllers/comment/commentCreate.php
+++ Controllers/comment/commentCreate.php
@@ -1,19 +1,31 @@
 <?php
 
+session_start();
+
 include_once '../../Objects/comment.php';
 include_once '../../Core/database.php';
+include_once '../../Objects/user.php';
 
-$database = new Database();
-$conn = $database->getConnection();
-$comment = new Comment($conn);
+if(isset($_SESSION['user']) && isset($_POST['link']) && isset($_POST['userId'])
+    && isset($_POST['topic']) && isset($_POST['body']))
 
-$linc = $_POST['link'];
-$userId = $_POST['userId'];
-$topicId = $_POST['topic'];
-$body = $_POST['body'];
-$comment->setTopic($topicId);
-$comment->setBody($body);
-$comment->setUserId($userId);
-$stmt = $comment->commentCreate();
+{
+    $link = $_POST['link'];
+    $userId = $_POST['userId'];
+    $topicId = $_POST['topic'];
+    $body = $_POST['body'];
 
-header('Location: '.$linc.'?topicId='.$topicId);
+    $database = new Database();
+    $conn = $database->getConnection();
+    $user = new User($conn);
+    $permission = $user->userHasPermission($userId, 4);
+    if(true === $permission){
+        $comment = new Comment($conn);
+        $comment->setTopic($topicId);
+        $comment->setBody($body);
+        $comment->setUserId($userId);
+        $stmt = $comment->commentCreate();
+    }
+}
+
+header('Location: '.$link.'?topicId='.$topicId);
