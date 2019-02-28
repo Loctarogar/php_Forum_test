3bb8e8e65f47bad66fd6487fafe23d3631192384
Loctarogar <igrsl1990@gmail.com>
2019-02-28 19:06:14 +0300

    topicRead

--- Controllers/topic/topicRead.php
+++ Controllers/topic/topicRead.php
@@ -8,20 +8,19 @@ include_once '../../Objects/user.php';
 $pageTitle = "Topic Read";
 if(isset($_GET['topicId'])){
     $id = $_GET['topicId'];
+    $database = new Database();
+    $conn = $database->getConnection();
+    $topic = new Topic($conn);
+    $stmtComments = $topic->topicGetComments($id);
+    $comments = $stmtComments->fetchAll();
+    $user = new User($conn);
+    $stmtTopic = $topic->topicRead($id);
+    $topic = $stmtTopic->fetch();
+    $stmtUser = $user->userRead($topic['user_id']);
+    $user = $stmtUser->fetch();
 }else{
-    $id = 5;
+    $message = "An error occurred";
 }
-
-$database = new Database();
-$conn = $database->getConnection();
-$topic = new Topic($conn);
-$stmtComments = $topic->topicGetComments($id);
-$comments = $stmtComments->fetchAll();
-$user = new User($conn);
-$stmtTopic = $topic->topicRead($id);
-$topic = $stmtTopic->fetch();
-$stmtUser = $user->userRead($topic['user_id']);
-$user = $stmtUser->fetch();
 
 include_once '../../Templates/layouts/header.php';
 include_once '../../Templates/topic/topicRead.php';
